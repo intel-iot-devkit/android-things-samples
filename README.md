8d2931fade9f9d0405325ee6ef8a2b4acb3ac349
Sanrio Alvares <sanrio.alvares@intel.com>
2017-03-29 02:47:13 -0700

    Add Jhd1313m1 LCD Example
    Signed-off-by: Sanrio Alvares <sanrio.alvares@intel.com>

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # android-things-samples
 Samples and projects for platforms running Android Things
+
+Note: Only supported on x86 platforms.
---
+++ jhd1313m1/src/main/java/com/example/upm/androidthings/driversamples/JhdActivity.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2017 Intel Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.upm.androidthings.driversamples;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+/**
+  * Example of using Grove Jhd1313m1 LCD.
+  *
+  * This activity initializes the LCD and displays RGB color combination with text.
+  *
+  */
+
+public class JhdActivity extends Activity {
+    private static final String TAG = "JhdActivity";
+    static {
+        try {
+            System.loadLibrary("javaupm_jhd1313m1");
+        } catch (UnsatisfiedLinkError e) {
+            System.err.println(
+                    "Native library failed to load.\n" + e);
+            System.exit(1);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "Starting JhdActivity");
+        // Instantiate an jhd1313m1 instance on I2C bus 0
+        try {
+            upm_jhd1313m1.Jhd1313m1 lcd =
+                new upm_jhd1313m1.Jhd1313m1(0, 0x3E, 0x62);
+
+            lcd.clear();
+            int ndx = 0;
+            short[][] rgb = new short[][]{
+                {0xd1, 0x00, 0x00},
+                {0xff, 0x66, 0x22},
+                {0xff, 0xda, 0x21},
+                {0x33, 0xdd, 0x00},
+                {0x11, 0x33, 0xcc},
+                {0x22, 0x00, 0x66},
+                {0x33, 0x00, 0x44}
+            };
+            
+	    while (true) {
+                // Alternate rows on the LCD
+                lcd.setCursor(ndx % 2, 0);
+                // Change the color
+                short r = rgb[ndx % 7][0];
+                short g = rgb[ndx % 7][1];
+                short b = rgb[ndx % 7][2];
+                lcd.setColor(r, g, b);
+                lcd.write("Hello World " + ndx);
+                // Echo via printf
+                System.out.println("Hello World" + ndx++);
+                System.out.format("rgb: 0x%02x%02x%02x\n", r, g, b);
+                Thread.sleep(1000);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error in UPM APIs", e);
+        }
+    }
+}
