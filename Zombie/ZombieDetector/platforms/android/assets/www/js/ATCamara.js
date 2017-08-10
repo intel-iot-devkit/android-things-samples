a5bf980cd1494689a43688e9de0b54526e383818
junweifu <junwei.fu@intel.com>
2017-08-10 16:49:58 +0800

    Create ZombieDetector project manually with cordova:template
    This ZombieDetector Cordova project included node modules that is too
    complex to pass IP Plan, so guide user Create ZombieDetector project manually.

--- Zombie/ZombieDetector/platforms/android/assets/www/js/ATCamara.js
+++
@@ -1,7 +0,0 @@
-/*global cordova, module*/
-
-module.exports = {
-    TakePhoto: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "ATCamara", "TakePhoto", [name]);
-    }
-};
--- Zombie/ZombieDetector/platforms/android/platform_www/js/ATTensorflow.js
+++
@@ -1,7 +0,0 @@
-/*global cordova, module*/
-
-module.exports = {
-    Classifier: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "ATTensorflow", "Classifier", [name]);
-    }
-};
